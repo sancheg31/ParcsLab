e02e3a933f91ab3e7577c26d7219b9edfdfe40fe
sancheg31 <45858435+sancheg31@users.noreply.github.com>
2021-11-26 03:38:11 +0200

    8

--- src/Palindrom.java
+++ src/Palindrom.java
@@ -8,75 +8,28 @@ import parcs.*;
 
 public class Palindrom implements AM
 {
-    public static void main(String[] args) throws Exception {
-
-        task currentTask = new task();
-        currentTask.addJarFile("Palindrom.jar");
-        currentTask.addJarFile("Solver.jar");
-
-        System.out.println("Waiting for result...");
-
-        Palindrom runner = new Palindrom();
-        runner.run(new AMInfo(currentTask, null));
-
-        System.out.println("Result: ");
-
-        currentTask.end();
-    }
-
     public void run(AMInfo info)
     {
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new File("input.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int start = info.parent.readInt();
+        int end = info.parent.readInt();
+
+        for (int number = start; number < end; ++number)
+        {
+            if (IsPalindrome(number))
+            {
+                result.add(number);
+            }
         }
 
-        int threads = scanner.nextInt();
-        int start = scanner.nextInt();
-        int end = scanner.nextInt();
-
-        long startTime = System.nanoTime();
-        List<Integer> palindromes = solve(info, threads, start, end);
-        long endTime = System.nanoTime();
-
-        System.out.println("Palindromes count: " + palindromes.size());
-        System.out.println("Time: " + ((endTime - startTime) / 1000000) + "ms");
+        info.parent.write(result);
     }
 
-    public List<Integer> solve(AMInfo info, int threads, int start, int end)
+    public boolean IsPalindrome(int number)
     {
-        List<point> points = new ArrayList<>();
-        List<channel> channels = new ArrayList<>();
-
-        int remainder = (end - start) % threads;
-        int length = (end - start) / threads;
-
-        for (int index = 0; index < threads; ++index)
-        {
-            int currentStart = index * length;
-            int currentEnd = (index + 1) * length + ((threads - index - 1 < remainder) ? 1 : 0);
-
-            point newPoint = info.createPoint();
-            channel newChannel = newPoint.createChannel();
-            channels.add(newChannel);
-            points.add(newPoint);
-
-            newPoint.execute("Solver");
-            newChannel.write(currentStart);
-            newChannel.write(currentEnd);
-        }
-
-        List<Integer> result = new ArrayList<>();
-        for (int index = 0; index < threads; ++index)
-        {
-            result.addAll((Collection<? extends Integer>) channels.get(index).readObject());
-        }
-
-        return result;
+        return false;
     }
-
 
 
 }
--- src/Solver.java
+++ src/Solver.java
@@ -7,27 +7,75 @@ import parcs.*;
 
 public class Solver implements AM
 {
+    public static void main(String[] args) throws Exception {
+
+        task currentTask = new task();
+        currentTask.addJarFile("Palindrom.jar");
+        currentTask.addJarFile("Solver.jar");
+
+        System.out.println("Waiting for result...");
+
+        Solver solver = new Solver();
+        solver.run(new AMInfo(currentTask, null));
+
+        System.out.println("Result: ");
+
+        currentTask.end();
+    }
+
     public void run(AMInfo info)
     {
-        ArrayList<Integer> result = new ArrayList<>();
-
-        int start = info.parent.readInt();
-        int end = info.parent.readInt();
-
-        for (int number = start; number < end; ++number)
-        {
-            if (IsPalindrome(number))
-            {
-                result.add(number);
-            }
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("input.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
-        info.parent.write(result);
+        int threads = scanner.nextInt();
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        long startTime = System.nanoTime();
+        List<Integer> palindromes = solve(info, threads, start, end);
+        long endTime = System.nanoTime();
+
+        System.out.println("Palindromes count: " + palindromes.size());
+        System.out.println("Time: " + ((endTime - startTime) / 1000000) + "ms");
     }
 
-    public boolean IsPalindrome(int number)
+    public List<Integer> solve(AMInfo info, int threads, int start, int end)
     {
-        return false;
+        List<point> points = new ArrayList<>();
+        List<channel> channels = new ArrayList<>();
+
+        int remainder = (end - start) % threads;
+        int length = (end - start) / threads;
+
+        for (int index = 0; index < threads; ++index)
+        {
+            int currentStart = index * length;
+            int currentEnd = (index + 1) * length + ((threads - index - 1 < remainder) ? 1 : 0);
+
+            point newPoint = info.createPoint();
+            channel newChannel = newPoint.createChannel();
+            channels.add(newChannel);
+            points.add(newPoint);
+
+            newPoint.execute("Palindrom");
+            newChannel.write(currentStart);
+            newChannel.write(currentEnd);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int index = 0; index < threads; ++index)
+        {
+            result.addAll((Collection<? extends Integer>) channels.get(index).readObject());
+        }
+
+        return result;
     }
+
+
 
 }
